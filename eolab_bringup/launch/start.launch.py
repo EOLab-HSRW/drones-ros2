40cb7b4f860b42e6e8c7e5abf50b0f957a668fe3
harleylara <harley.lara@outlook.com>
2025-05-31 01:35:32 +0200

    refactor single agent bringup

---
+++ eolab_bringup/launch/start.launch.py
@@ -0,0 +1,160 @@
+from os import environ
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, LogInfo, OpaqueFunction, RegisterEventHandler, SetEnvironmentVariable, GroupAction
+from launch_testing.event_handlers import StdoutReadyListener
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+import xacro
+import eolab_drones
+
+def launch_args(context):
+
+    declared_args = []
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="drone",
+            default_value="protoflyer",
+            description="Name of the drone to launch"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="x",
+            default_value="0.0",
+            description="X position to spawn the drone in sim"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="y",
+            default_value="0.0",
+            description="Y position to spawn the drone in sim"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="z",
+            default_value="0.0",
+            description="Z position to spawn the drone in sim"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="lat",
+            default_value="51.497741558866004",
+            description="GPS Coordinate Latitude in WGS84"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="lon",
+            default_value="6.549182534441797",
+            description="GPS Coordinate Longitude in WGS84"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="alt",
+            default_value="26.54",
+            description="GPS Coordinate Altitude in WGS84"
+        )
+    )
+
+    declared_args.append(
+        DeclareLaunchArgument(
+            name="world",
+            default_value="empty",
+            description="Name of the world to launch (without file extension). Only the ones under the `worlds` folder in the pkg `eolab_description`."
+        )
+    )
+
+    return declared_args
+
+
+def launch_setup(context):
+
+
+    gazebo_world = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare("eolab_bringup"), "launch", "start_world.launch.py"])
+    )
+
+    drone_name = LaunchConfiguration("drone").perform(context)
+    frame_id = eolab_drones.get_frame_id(drone_name)
+
+    start_px4 = ExecuteProcess(
+        name="px4_sitl",
+        cmd=[f"/home/harley/eolab-git/drones-fw/PX4-Autopilot/build/px4_sitl_{drone_name}/bin/px4", "-i", drone_name],
+        output="both",
+        additional_env={
+            "SYSTEM": "gz",
+            "PX4_GZ_STANDALONE": "1",
+            "PX4_UXRCE_DDS_NS": f"{drone_name}1",
+            "PX4_SYS_AUTOSTART": frame_id,
+            "PX4_SIMULATOR": "gz",
+            "PX4_GZ_WORLD": LaunchConfiguration("world"),
+            "PX4_GZ_MODEL_NAME": f"{drone_name}1",
+        }
+    )
+
+
+    robot_desc_content = xacro.process_file(
+        PathJoinSubstitution(
+            [FindPackageShare("eolab_description"), "drones", f"{drone_name}.urdf.xacro"]
+        ).perform(context)
+    ).toxml().replace("\n", "") # IMPOTANT TO FLAT the string to remove newline characters
+
+    spawn_drone = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-name", f"{drone_name}1",
+            "-string", robot_desc_content,
+            "-x", LaunchConfiguration("x"),
+            "-y", LaunchConfiguration("y"),
+            "-z", LaunchConfiguration("z"),
+            # "-world", LaunchConfiguration("world") # TODO: check if required
+        ],
+        output='both'
+    )
+
+    start_agent = ExecuteProcess(
+        cmd=["MicroXRCEAgent", "udp4", "-p", "8888"],
+        output="both"
+    )
+
+    wait_for_sitl_ready = RegisterEventHandler(
+        StdoutReadyListener(
+            target_action=start_px4,
+            ready_txt="INFO  [init] Standalone PX4 launch, waiting for Gazebo",
+            actions=[
+                LogInfo(msg="Got SITL firmware running..."),
+                gazebo_world,
+                spawn_drone,
+                start_agent
+            ]
+        )
+    )
+
+
+    return [
+        start_px4,
+        wait_for_sitl_ready,
+    ]
+
+
+def generate_launch_description() -> LaunchDescription:
+
+    ld = LaunchDescription()
+
+    ld.add_action(OpaqueFunction(function=launch_args))
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
+    return ld
---
+++ eolab_bringup/launch/start_world.launch.py
@@ -0,0 +1,102 @@
+from os import environ
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, OpaqueFunction, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def launch_setup(context):
+
+    # TODO harley: this is just a quick fix to export
+    # GZ_SIM_RESOURCE_PATH to point to the `assets` folder of this package.
+    # this is due to problems with the export tag `gazebo_ros` inside the package.xml in eolab_description
+    # the export function to GZ_SIM_RESOURCE_PATH was introduced in 0.244.14
+    # but current debian pkg (2025 May) is version 0.244.12 only export to IGN_SIM_RESOURCE_PATH making gazebo 8
+    # not able to find the resources
+    export_assets = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        value=(environ.get("GZ_SIM_RESOURCE_PATH", default="") + ":" + PathJoinSubstitution([FindPackageShare("eolab_description"), ".."]).perform(context))
+    )
+
+    # TODO get the path to the SITL dynamically based on the drones name
+    export_plugins = SetEnvironmentVariable(
+        "GZ_SIM_SYSTEM_PLUGIN_PATH",
+        value=(environ.get("GZ_SIM_SYSTEM_PLUGIN_PATH", default="") + ":" + r"/home/harley/eolab-git/drones-fw/PX4-Autopilot/build/px4_sitl_protoflyer/src/modules/simulation/gz_plugins")
+    )
+
+    export_server_config = SetEnvironmentVariable(
+        "GZ_SIM_SERVER_CONFIG_PATH",
+        value=(environ.get("GZ_SIM_SERVER_CONFIG_PATH", default=PathJoinSubstitution([FindPackageShare("eolab_description"), "server.config"]).perform(context)))
+    )
+
+    set_gps_coord = ExecuteProcess(
+        name="set_gps_coord",
+        cmd=[
+            "gz", "service", "-s", f"/world/{LaunchConfiguration('world').perform(context)}/set_spherical_coordinates",
+            "--reqtype", "gz.msgs.SphericalCoordinates",
+            "--reptype", "gz.msgs.Boolean",
+            "--timeout", "5000",
+            "--req", f"surface_model: EARTH_WGS84, latitude_deg: {LaunchConfiguration('lat').perform(context)}, longitude_deg: {LaunchConfiguration('lon').perform(context)}, elevation: {LaunchConfiguration('alt').perform(context)}",
+        ]
+    )
+
+    gz = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare("ros_gz_sim"),
+            "launch",
+            "gz_sim.launch.py"
+        ]),
+        launch_arguments={
+            "gz_args": ["-r ", PathJoinSubstitution([FindPackageShare("eolab_description"), "worlds", f"{LaunchConfiguration('world').perform(context)}.sdf"]), " -v"],
+            "gz_version": "8",
+            "on_exit_shutdown": "True",
+        }.items()
+    )
+
+    return [
+        export_assets,
+        export_plugins,
+        export_server_config,
+        set_gps_coord,
+        gz
+    ]
+
+
+def generate_launch_description() -> LaunchDescription:
+
+    ld = LaunchDescription()
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            name="world",
+            default_value="empty",
+            description="World file in the `world` folder in eolab_description."
+        )
+    )
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            name="lat",
+            default_value="51.497741558866004",
+            description="GPS Coordinate Latitude in WGS84"
+        )
+    )
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            name="lon",
+            default_value="6.549182534441797",
+            description="GPS Coordinate Longitude in WGS84"
+        )
+    )
+
+    ld.add_action(
+        DeclareLaunchArgument(
+            name="alt",
+            default_value="26.54",
+            description="GPS Coordinate Altitude in WGS84"
+        )
+    )
+
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
+    return ld
